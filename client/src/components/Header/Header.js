c6a9e70357fbbe7e8b8e9c7f2da5a07f55cbe81c
Sams <nicholettesams@msn.com>
2018-12-26 22:41:22 -0500

    Basic header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+const Header = () =>
+  <div className="page-header text-center ">
+   <h1>
+      New York Times Article Scrubber<br/>
+      <h3>Search for and annotate articles of interest!</h3>
+   </h1>
+   
+</div>;
+
+export default Header;
