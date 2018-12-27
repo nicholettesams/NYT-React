79aad38946b9597bc4d8be6a635da5d441a4caef
Sams <nicholettesams@msn.com>
2018-12-26 23:24:38 -0500

    Search entry form

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
 import "./Form.css";
 
-class Form extends Component {
+class Search extends Component {
   // Setting the component's initial state
   state = {
-    firstName: "",
-    lastName: "", 
-    password: ""
+    topic: "",
+    startYear: "", 
+    endYear: ""
   };
 
   handleInputChange = event => {
     // Getting the value and name of the input which triggered the change
-    //const value = event.target.value;
-    //const name = event.target.name;
     const { name, value } = event.target;
-
-    if (name === "password") {
-      return this.setState({
-        password: value.substring(0, 15)
-      })
-    }
 
     // Updating the input's state
     return this.setState({
@@ -31,21 +23,17 @@ class Form extends Component {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
 
-    // validation
-    if(!this.state.firstName || !this.state.lastName) {
-      return alert("First and Last Name must not be blank.")
-    }
+   // TODO: call the api to get the articles.  Pass the parameters needed
+   // this.state.topic
+   // this.state.startYear
+   // this.state.endYear
 
-    if(this.state.password.length < 6){
-      return alert("Password must be at least 6 characters.")
-    }
+   // Handle results?
 
-    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-    alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
     this.setState({
-      firstName: "",
-      lastName: "",
-      password: ""
+      topic: "",
+      startYear: "",
+      endYear: ""
     });
   };
 
@@ -53,31 +41,37 @@ class Form extends Component {
     // Notice how each input has a `value`, `name`, and `onChange` prop
     return (
       <div>
-        <p>
-          Hello {this.state.firstName} {this.state.lastName}
-        </p>
-        <form className="form">
-          <input
-            value={this.state.firstName}
-            name="firstName"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="First Name"
-          />
-          <input
-            value={this.state.lastName}
-            name="lastName"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Last Name"
-          />
-          <input
-            value={this.state.password}
-            name="password"
-            onChange={this.handleInputChange}
-            type="password"
-          />
-          <button onClick={this.handleFormSubmit}>Submit</button>
+        <form onSubmit={this.handleFormSubmit}>
+          <div className="form-group">
+            <label htmlFor="topic">Topic:</label><br/>
+            <input
+              value={this.state.topic}
+              onChange={this.handleInputChange}
+              name="topic"
+              id="topic"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="startyear">Start Year:</label><br/>
+            <input 
+              value={this.state.startyear}
+              onChange={this.handleInputChange}
+              name="startyear"
+              placeholder="YYYY" 
+              id="startyear"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="endyear">End Year:</label><br/>
+            <input 
+              value={this.state.endyear}
+              onChange={this.handleInputChange}
+              name="endyear"
+              placeholder="YYYY"
+              id="endyear"
+            />
+          </div>
+          <button type="submit" className="btn">Search</button>
         </form>
       </div>
     );
