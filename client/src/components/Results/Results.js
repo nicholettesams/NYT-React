8f8635eadc5f933278a16faa98e635b66296a666
Sams <nicholettesams@msn.com>
2018-12-26 23:31:22 -0500

    Basic results and saved sections

---
+++ client/src/components/Results/Results.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Results = props => {
+    return (
+        <div>
+            <div className="panel-heading">
+                <h1>Results</h1>
+            </div>
+            <div className="panel-body">
+            </div>
+        </div>
+    )};
+
+export default Results;
---
+++ client/src/components/Saved/Saved.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Saved = props => {
+    return (
+        <div>
+            <div className="panel-heading">
+                <h1>Saved</h1>
+            </div>
+            <div className="panel-body">
+            </div>
+        </div>
+    )};
+
+export default Saved;
