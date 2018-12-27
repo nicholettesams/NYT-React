7612f1fa82f0f215e1b19323d1667e8e933e4b6b
Sams <nicholettesams@msn.com>
2018-12-27 00:29:01 -0500

    fix header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,12 @@
 
 import React from "react";
 
-const Header = () =>
-  <div className="page-header text-center ">
-   <h1>
-      New York Times Article Scrubber<br/>
-      <h3>Search for and annotate articles of interest!</h3>
-   </h1>
-   
-</div>;
+const Header = () => {
+   return(
+      <div className="page-header text-center ">
+         <h1>New York Times Article Scrubber<br/></h1>
+         <h3>Search for and annotate articles of interest!</h3>
+      </div>
+   )};
 
 export default Header;
